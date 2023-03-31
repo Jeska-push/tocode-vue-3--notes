a112715ec2f34ef39103bfaede21c73df3597b58
Jeska-push <jesika_@bk.ru>
2023-03-31 15:26:48 +0300

    test3

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import routers from "./routes"
 import { store } from "./store"
-
 import fetch from 'node-fetch'
 import "./assets/scss/main.scss"
-<script src="/tocode-vue-3--notes/tocode-vue-3--notes/src/utils/local.js"></script>
-//import { updateLocalStorage } from "./src/utils/local.js"
 const app = createApp(App);
 app.use(routers)
 app.use(fetch)
